1e1faba955beb6df8d2e3eb66367dab8d949f9c1
Lucas Araujo <lucasaraujo1@ufpr.br>
2024-04-19 17:39:15 -0300

    improved movement and added vector usage

--- components/game/game.js
+++ components/game/game.js
@@ -2,127 +2,126 @@
 var gravity = 0.98;
 var jumpForce = 20;
 var groundLevel = 0;
-var maxSpeed = 25;
-var baseAcceleration = 2.5;
+var maxSpeed = 10;
+var forceBase = 1;
+var forceMultiplier = 2.5;
 var Character = /** @class */ (function () {
     function Character() {
+        this.size = 32;
+        this.pos = { x: 100, y: 1000 };
+        this.vel = { x: 0, y: 0 };
+        this.forceX = 0;
+        this.isMovingKeyDown = false;
+        this.isStopped = false;
+        this.sign = 1;
         this.element = document.createElement("div");
         this.element.className = "character";
-        this.size = 50;
         this.element.style.width = "".concat(this.size, "px");
         this.element.style.height = "".concat(this.size, "px");
-        this.x = 100;
-        this.y = 1000;
-        this.vX = 0;
-        this.vY = 0;
-        this.acceleration = 0;
-        this.isMoving = false;
-        this.isJumping = false;
         this.updateElementPos();
         this.listeners();
     }
     Character.prototype.listeners = function () {
         var _this = this;
         document.addEventListener("keydown", function (e) {
-            if (e.key === " " && !_this.isJumping) {
+            if (e.key === " ") {
                 _this.jump();
             }
             else if (e.key === "ArrowRight") {
-                _this.move(true);
+                _this.moveStep(true);
+                _this.isMovingKeyDown = true;
             }
             else if (e.key === "ArrowLeft") {
-                _this.move(false);
+                _this.moveStep(false);
+                _this.isMovingKeyDown = true;
             }
         });
         document.addEventListener("keyup", function (e) {
             if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
-                _this.disaccelerate();
+                _this.isMovingKeyDown = false;
             }
         });
+    };
+    Character.prototype.onGround = function () {
+        return this.pos.y <= groundLevel;
     };
     Character.prototype.getElement = function () {
         return this.element;
     };
     Character.prototype.updateElementPos = function () {
-        this.element.style.left = "".concat(this.x, "px");
-        this.element.style.bottom = "".concat(this.y, "px");
+        this.element.style.left = "".concat(this.pos.x, "px");
+        this.element.style.bottom = "".concat(this.pos.y, "px");
     };
     Character.prototype.updatePosition = function () {
-        this.x += this.vX;
-        this.y += this.vY;
+        this.pos.x += this.vel.x;
+        this.pos.y += this.vel.y;
         this.updateElementPos();
     };
+    Character.prototype.getSize = function () { return this.size; };
     Character.prototype.setPos = function (x, y) {
-        this.x = x;
-        this.y = y;
+        this.setX(x);
+        this.setY(y);
     };
-    Character.prototype.getSize = function () {
-        return this.size;
-    };
-    Character.prototype.getY = function () {
-        return this.y;
-    };
-    Character.prototype.getX = function () {
-        return this.x;
-    };
-    Character.prototype.setY = function (y) {
-        this.y = y;
-    };
-    Character.prototype.setX = function (x) {
-        this.x = x;
-    };
+    Character.prototype.getX = function () { return this.pos.x; };
+    Character.prototype.getY = function () { return this.pos.y; };
+    Character.prototype.setX = function (x) { this.pos.x = x; };
+    Character.prototype.setY = function (y) { this.pos.y = y; };
     Character.prototype.jump = function () {
-        if (this.y === groundLevel) {
-            this.isJumping = true;
-            this.vY = jumpForce;
-            this.y += 1; // to avoid instant gravity
+        if (this.onGround()) {
+            this.vel.y = jumpForce;
+            this.pos.y++;
         }
+    };
+    Character.prototype.moveStep = function (toRight) {
+        this.sign = toRight ? 1 : -1;
+        this.isStopped = false;
+        this.forceX = forceBase * this.sign;
     };
     Character.prototype.move = function (toRight) {
-        this.isMoving = true;
-        this.acceleration = toRight ? baseAcceleration : -baseAcceleration;
-        this.vX += this.acceleration;
-        if (Math.abs(this.vX) > maxSpeed) {
-            this.vX = toRight ? maxSpeed : -maxSpeed;
-        }
+        if (this.isStopped)
+            return;
+        this.sign = toRight ? 1 : -1;
+        // TODO: Refactor
+        // - Need to create an accerelation vector
+        // - Acceleration vector should be multiplied by the time passed
+        // - Increase acceleration with time
+        // - Start decelerating when the key is released
+        if (this.isMovingKeyDown)
+            this.vel.x += this.forceX;
+        this.forceX *= forceMultiplier;
+        if (Math.abs(this.vel.x) > maxSpeed)
+            this.vel.x = maxSpeed * this.sign;
     };
     Character.prototype.stop = function () {
-        this.vX = 0;
+        this.isStopped = true;
+        this.forceX = 0;
+        this.vel.x = 0;
     };
     Character.prototype.disaccelerate = function () {
-        this.isMoving = false;
-        this.acceleration -= (this.acceleration > 0 ? baseAcceleration : -baseAcceleration) * 3;
-        this.vX *= 0.7;
-        if (Math.abs(this.vX) < 0.1) {
+        if (this.isMovingKeyDown)
+            return;
+        this.forceX /= forceMultiplier;
+        this.vel.x *= 0.5;
+        if (Math.abs(this.forceX) < 0.1)
             this.stop();
-        }
     };
     Character.prototype.gravityForce = function () {
-        if (this.y <= groundLevel) {
-            this.vY = 0;
-            this.y = groundLevel;
-            this.isJumping = false;
+        if (this.onGround()) {
+            this.vel.y = 0;
+            this.pos.y = groundLevel;
             return;
         }
-        this.vY -= gravity;
-        if (this.vY < -jumpForce) {
-            this.vY = -jumpForce;
-        }
+        this.vel.y -= gravity;
     };
     Character.prototype.pushAway = function (limit, multiplier) {
-        if (multiplier === void 0) { multiplier = 15; }
-        this.x = limit;
-        this.vX *= -multiplier;
-        this.updateElementPos();
+        if (multiplier === void 0) { multiplier = 2; }
+        this.pos.x = limit;
+        this.vel.x *= -multiplier;
     };
     Character.prototype.update = function () {
         this.gravityForce();
-        if (this.isMoving) {
-            this.move(this.acceleration > 0);
-        }
-        else if (this.acceleration !== 0 && Math.abs(this.vX) > 0.1) {
-            this.disaccelerate();
-        }
+        this.disaccelerate();
+        this.move(this.sign > 0);
         this.updatePosition();
     };
     return Character;
--- components/game/game.ts
+++ components/game/game.ts
@@ -1,155 +1,166 @@
 const gravity: number = 0.98;
 const jumpForce: number = 20;
 const groundLevel: number = 0;
-const maxSpeed: number = 25;
-const baseAcceleration: number = 2.5;
+const maxSpeed: number = 10;
+const forceBase: number = 1;
+const forceMultiplier: number = 2.5;
+
+interface Vector {
+    x: number;
+    y: number;
+}
 
 class Character {
 	private element: HTMLElement;
 	private size: number;
-	private x: number;
-	private y: number;
-	private vX: number;
-	private vY: number;
-	private acceleration: number;
-	private isMoving: boolean;
-	private isJumping: boolean;
+	// private x: number;
+	// private y: number;
+    private pos: Vector;
+	// private vX: number;
+	// private vY: number;
+    private vel: Vector;
+	private forceX: number;
+	private isMovingKeyDown: boolean;
+    private isStopped: boolean;
+    private sign: number;
 
 	constructor() {
+		this.size = 32;
+        this.pos = { x: 100, y: 1000 };
+        this.vel = { x: 0, y: 0 };
+		this.forceX = 0;
+		this.isMovingKeyDown = false;
+        this.isStopped = false;
+        this.sign = 1;
+
 		this.element = document.createElement("div");
 		this.element.className = "character";
-		this.size = 50;
 		this.element.style.width = `${this.size}px`;
 		this.element.style.height = `${this.size}px`;
-		this.x = 100;
-		this.y = 1000;
-		this.vX = 0;
-		this.vY = 0;
-		this.acceleration = 0;
-		this.isMoving = false;
-		this.isJumping = false;
 		this.updateElementPos();
+
 		this.listeners();
 	}
 
 	listeners() {
 		document.addEventListener("keydown", e => {
-			if (e.key === " " && !this.isJumping) {
+			if (e.key === " ") {
 				this.jump();
 			} else if (e.key === "ArrowRight") {
-				this.move(true);
+                this.moveStep(true);
+                this.isMovingKeyDown = true;
 			} else if (e.key === "ArrowLeft") {
-				this.move(false);
+				this.moveStep(false);
+                this.isMovingKeyDown = true;
 			}
 		});
 
 		document.addEventListener("keyup", e => {
 			if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
-				this.disaccelerate();
+                this.isMovingKeyDown = false;
 			}
 		});
 	}
 
-	getElement() {
+    private onGround(): boolean {
+        return this.pos.y <= groundLevel;
+    }
+
+	getElement(): HTMLElement {
 		return this.element;
 	}
 
-	updateElementPos() {
-		this.element.style.left = `${this.x}px`;
-		this.element.style.bottom = `${this.y}px`;
+	updateElementPos(): void {
+		this.element.style.left = `${this.pos.x}px`;
+		this.element.style.bottom = `${this.pos.y}px`;
 	}
 
-	updatePosition() {
-		this.x += this.vX;
-		this.y += this.vY;
+	updatePosition(): void {
+		this.pos.x += this.vel.x;
+		this.pos.y += this.vel.y;
 		this.updateElementPos();
 	}
 
+	getSize(): number { return this.size; }
+
 	setPos(x: number, y: number) {
-		this.x = x;
-		this.y = y;
+        this.setX(x);
+        this.setY(y);
 	}
 
-	getSize() {
-		return this.size;
-	}
+	getX(): number { return this.pos.x; }
+    getY(): number { return this.pos.y; }
 
-	getY() {
-		return this.y;
-	}
-
-	getX() {
-		return this.x;
-	}
-
-	setY(y: number) {
-		this.y = y;
-	}
-
-	setX(x: number) {
-		this.x = x;
-	}
+    setX(x: number) { this.pos.x = x; }
+	setY(y: number) { this.pos.y = y; }
 
 	jump() {
-		if (this.y === groundLevel) {
-			this.isJumping = true;
-			this.vY = jumpForce;
-            this.y += 1; // to avoid instant gravity
+		if (this.onGround()) {
+			this.vel.y = jumpForce;
+            this.pos.y++;
 		}
 	}
 
+    moveStep(toRight: boolean) {
+        this.sign = toRight ? 1 : -1;
+        this.isStopped = false;
+        this.forceX = forceBase * this.sign;
+    }
+
 	move(toRight: boolean) {
-		this.isMoving = true;
-		this.acceleration = toRight ? baseAcceleration : -baseAcceleration;
-		this.vX += this.acceleration;
-		if (Math.abs(this.vX) > maxSpeed) {
-			this.vX = toRight ? maxSpeed : -maxSpeed;
-		}
+        if (this.isStopped) return;
+
+        this.sign = toRight ? 1 : -1;
+
+        // TODO: Refactor
+        // - Need to create an accerelation vector
+        // - Acceleration vector should be multiplied by the time passed
+        // - Increase acceleration with time
+        // - Start decelerating when the key is released
+        if (this.isMovingKeyDown)
+            this.vel.x += this.forceX;
+
+        this.forceX *= forceMultiplier;
+
+        if (Math.abs(this.vel.x) > maxSpeed)
+            this.vel.x = maxSpeed * this.sign;
 	}
 
 	stop() {
-		this.vX = 0;
+        this.isStopped = true;
+        this.forceX = 0;
+        this.vel.x = 0;
 	}
 
 	disaccelerate() {
-		this.isMoving = false;
+        if (this.isMovingKeyDown) return;
 
-		this.acceleration -= (this.acceleration > 0 ? baseAcceleration : -baseAcceleration) * 3;
-
-		this.vX *= 0.7;
-		if (Math.abs(this.vX) < 0.1) {
-			this.stop();
-		}
+        this.forceX /= forceMultiplier;
+		this.vel.x *= 0.5;
+        if (Math.abs(this.forceX) < 0.1)
+            this.stop();
 	}
 
 	gravityForce() {
-		if (this.y <= groundLevel) {
-			this.vY = 0;
-			this.y = groundLevel;
-			this.isJumping = false;
+		if (this.onGround()) {
+			this.vel.y = 0;
+			this.pos.y = groundLevel;
             return;
 		}
 
-		this.vY -= gravity;
-        if (this.vY < -jumpForce) {
-            this.vY = -jumpForce;
-        }
+		this.vel.y -= gravity;
 	}
 
-	pushAway(limit: number, multiplier: number = 15) {
-		this.x = limit;
-		this.vX *= -multiplier;
-		this.updateElementPos();
+	pushAway(limit: number, multiplier: number = 2) {
+		this.pos.x = limit;
+		this.vel.x *= -multiplier;
 	}
 
 	update() {
 		this.gravityForce();
+        this.disaccelerate();
+        this.move(this.sign > 0);
 
-		if (this.isMoving) {
-			this.move(this.acceleration > 0);
-		} else if (this.acceleration !== 0 && Math.abs(this.vX) > 0.1) {
-			this.disaccelerate();
-		}
 		this.updatePosition();
 	}
 }
