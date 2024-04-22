0f16906fc393f1aa63a13dd6e354ad36a375f4e8
Lucas Araujo (Home) <lucasaraujo1@ufpr.br>
2024-04-22 18:28:45 -0300

    version 0.2 - game component - canvas implementation

--- components/game/game.js
+++ components/game/game.js
@@ -1,11 +1,10 @@
 "use strict";
 var gravity = 0.98;
-var jumpForce = 20;
-var groundLevel = 0;
+var jumpForce = 25;
 var maxSpeed = 10;
-var baseSpeed = 6;
-var forceIncrement = 0.1;
-var maxForce = 1;
+var baseSpeed = 5;
+var forceIncrement = 0.2;
+var maxForce = 2;
 var airTurnSpeed = 3;
 var DirectionX;
 (function (DirectionX) {
@@ -13,94 +12,135 @@ var DirectionX;
     DirectionX[DirectionX["Stop"] = 0] = "Stop";
     DirectionX[DirectionX["Right"] = 1] = "Right";
 })(DirectionX || (DirectionX = {}));
+var KeyCodes;
+(function (KeyCodes) {
+    KeyCodes[KeyCodes["Space"] = 32] = "Space";
+    KeyCodes[KeyCodes["ArrowRight"] = 39] = "ArrowRight";
+    KeyCodes[KeyCodes["ArrowLeft"] = 37] = "ArrowLeft";
+})(KeyCodes || (KeyCodes = {}));
 var Character = /** @class */ (function () {
-    function Character() {
-        this.size = 32;
-        this.pos = { x: 100, y: 1000 };
+    function Character(canvas, pos, color, size) {
+        if (pos === void 0) { pos = { x: 100, y: 1000 }; }
+        if (color === void 0) { color = "red"; }
+        if (size === void 0) { size = 32; }
+        this.canvas = canvas;
+        this.color = color;
+        this.pos = pos;
+        this.size = size;
         this.vel = { x: 0, y: 0 };
         this.forceX = 0;
-        this.isMovingKeyDown = false;
+        this.keys = { space: false, arrowRight: false, arrowLeft: false };
         this.isStopped = false;
         this.sign = 1;
         this.lastDirection = DirectionX.Stop;
-        this.element = document.createElement("div");
-        this.element.className = "character";
-        this.element.style.width = "".concat(this.size, "px");
-        this.element.style.height = "".concat(this.size, "px");
-        this.updateElementPos();
+        this.groundLevel = this.canvas.height - this.size;
+        this.update();
         this.listeners();
     }
     Character.prototype.listeners = function () {
         var _this = this;
         document.addEventListener("keydown", function (e) {
-            if (e.key === " ") {
-                _this.jump();
-            }
+            if (e.key === " ")
+                _this.keys.space = true;
             else if (e.key === "ArrowRight") {
-                _this.move(DirectionX.Right);
+                _this.keys.arrowRight = true;
             }
             else if (e.key === "ArrowLeft") {
-                _this.move(DirectionX.Left);
+                _this.keys.arrowLeft = true;
             }
         });
         document.addEventListener("keyup", function (e) {
-            if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
-                _this.isMovingKeyDown = false;
+            if (e.key === " ") {
+                _this.keys.space = false;
+            }
+            else if (e.key === "ArrowRight") {
+                _this.keys.arrowRight = false;
+            }
+            else if (e.key === "ArrowLeft") {
+                _this.keys.arrowLeft = false;
             }
         });
     };
-    Character.prototype.onGround = function () {
-        return this.pos.y <= groundLevel;
+    Character.prototype.update = function () {
+        if (this.keys.space)
+            this.jump();
+        if (this.isBothKeysPressed())
+            this.stop();
+        else if (this.keys.arrowLeft)
+            this.move(DirectionX.Left);
+        else if (this.keys.arrowRight)
+            this.move(DirectionX.Right);
+        this.nextPos();
+        this.draw();
     };
-    Character.prototype.getElement = function () {
-        return this.element;
+    Character.prototype.getSize = function () {
+        return this.size;
     };
-    Character.prototype.updateElementPos = function () {
-        this.element.style.left = "".concat(this.pos.x, "px");
-        this.element.style.bottom = "".concat(this.pos.y, "px");
+    Character.prototype.getPos = function () {
+        return this.pos;
     };
-    Character.prototype.updatePosition = function () {
-        this.pos.x += this.vel.x;
-        this.pos.y += this.vel.y;
-        this.updateElementPos();
-    };
-    Character.prototype.getSize = function () { return this.size; };
     Character.prototype.setPos = function (x, y) {
         this.setX(x);
         this.setY(y);
     };
-    Character.prototype.getX = function () { return this.pos.x; };
-    Character.prototype.getY = function () { return this.pos.y; };
-    Character.prototype.setX = function (x) { this.pos.x = x; };
-    Character.prototype.setY = function (y) { this.pos.y = y; };
+    Character.prototype.getX = function () {
+        return this.pos.x;
+    };
+    Character.prototype.getY = function () {
+        return this.pos.y;
+    };
+    Character.prototype.setX = function (x) {
+        this.pos.x = x;
+    };
+    Character.prototype.setY = function (y) {
+        this.pos.y = y;
+    };
+    Character.prototype.onGround = function () {
+        return this.pos.y >= this.groundLevel;
+    };
+    Character.prototype.draw = function () {
+        var ctx = this.canvas.getContext("2d");
+        if (!ctx)
+            throw new Error("Canvas not found");
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.pos.x, this.pos.y, this.size, this.size);
+    };
+    Character.prototype.nextPos = function () {
+        this.pos.x += this.vel.x;
+        this.pos.y -= this.vel.y;
+        this.disaccelerate();
+        this.gravityForce();
+    };
     Character.prototype.jump = function () {
         if (this.onGround()) {
             this.vel.y = jumpForce;
-            this.pos.y++;
+            this.pos.y--;
         }
     };
     Character.prototype.moveStep = function (direction) {
         this.isStopped = false;
-        this.isMovingKeyDown = true;
         this.lastDirection = direction;
         this.sign = direction;
         this.vel.x = baseSpeed * this.sign;
         this.forceX = forceIncrement * this.sign;
     };
     Character.prototype.move = function (direction) {
+        // First move, needs a first step
+        // like everything in life
         if (this.isStopped) {
             this.moveStep(direction);
             return;
         }
-        // If the character is in the air and the direction is different
-        // from the last direction, change the direction and reduce the speed
-        // to simulate air friction
-        if (!this.onGround() && direction !== this.lastDirection) {
-            this.vel.x = airTurnSpeed;
-            return;
-        }
+        // If the direction is different from the last direction
+        // you need to a new step (friction)
         if (direction !== this.lastDirection) {
             this.moveStep(direction);
+            // If in the air, reduce the speed to simulate
+            // air friction (you can't stop instantly in the air)
+            if (!this.onGround()) {
+                this.vel.x = airTurnSpeed * this.sign;
+                return;
+            }
             return;
         }
         // Limit speed
@@ -108,6 +148,8 @@ var Character = /** @class */ (function () {
             this.vel.x = maxSpeed * this.sign;
             return;
         }
+        // Check if the moviment is on pressed key same direction
+        this.validateDirection();
         // Increase force
         this.forceX += forceIncrement * this.sign;
         // Limit force
@@ -122,8 +164,26 @@ var Character = /** @class */ (function () {
         this.forceX = 0;
         this.vel.x = 0;
     };
+    Character.prototype.validateDirection = function () {
+        // Check velocity and key pressed
+        if (this.vel.x < 0 && !this.keys.arrowLeft ||
+            this.vel.x > 0 && !this.keys.arrowRight) {
+            if (this.keys.arrowLeft)
+                this.move(DirectionX.Left);
+            else if (this.keys.arrowRight)
+                this.move(DirectionX.Right);
+            else
+                this.stop();
+        }
+    };
+    Character.prototype.isMovingKeyPressed = function () {
+        return this.keys.arrowLeft || this.keys.arrowRight;
+    };
+    Character.prototype.isBothKeysPressed = function () {
+        return this.keys.arrowLeft && this.keys.arrowRight;
+    };
     Character.prototype.disaccelerate = function () {
-        if (this.isMovingKeyDown)
+        if (this.isMovingKeyPressed())
             return;
         this.forceX = 0;
         this.vel.x *= 0.3;
@@ -133,8 +193,7 @@ var Character = /** @class */ (function () {
     Character.prototype.gravityForce = function () {
         if (this.onGround()) {
             this.vel.y = 0;
-            this.pos.y = groundLevel;
-            return;
+            this.pos.y = this.groundLevel;
         }
         this.vel.y -= gravity;
     };
@@ -142,44 +201,43 @@ var Character = /** @class */ (function () {
         if (multiplier === void 0) { multiplier = 5; }
         this.pos.x = limit;
         this.stop();
-        this.updateElementPos();
-    };
-    Character.prototype.update = function () {
-        this.gravityForce();
-        this.disaccelerate();
-        this.updatePosition();
     };
     return Character;
 }());
 var Game = /** @class */ (function () {
     function Game() {
-        this.element = document.createElement("div");
-        this.element.id = "gameContainer";
-        document.body.appendChild(this.element);
-        this.character = new Character();
-        this.element.appendChild(this.character.getElement());
+        this.canvas = document.createElement("canvas");
+        this.canvas.id = "gameContainer";
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.ctx = null;
+        this.character = new Character(this.canvas);
+        this.leftLimit = 50;
+        this.rightLimit = window.innerWidth - this.leftLimit;
     }
     Game.prototype.start = function () {
         var _this = this;
-        setInterval(function () {
-            _this.update();
-        }, 1000 / 60);
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext("2d");
+        if (!this.ctx)
+            throw new Error("Canvas not found");
+        setInterval(function () { return _this.update(); }, 10);
     };
     Game.prototype.checkLimits = function () {
-        this.leftLimit = 50;
-        this.rightLimit = window.innerWidth - this.character.getSize();
-        this.rightLimit -= this.leftLimit;
+        var size = this.character.getSize();
         if (this.character.getX() < this.leftLimit)
             this.character.pushAway(this.leftLimit);
-        else if (this.character.getX() > this.rightLimit)
-            this.character.pushAway(this.rightLimit);
+        else if (this.character.getX() + size > this.rightLimit)
+            this.character.pushAway(this.rightLimit - size);
     };
     Game.prototype.update = function () {
-        this.character.update();
-        if (this.character.getY() < groundLevel) {
-            this.character.setY(groundLevel);
-        }
+        var _a;
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.rightLimit = window.innerWidth - this.leftLimit;
+        (_a = this.ctx) === null || _a === void 0 ? void 0 : _a.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.checkLimits();
+        this.character.update();
     };
     return Game;
 }());
--- components/game/game.ts
+++ components/game/game.ts
@@ -1,178 +1,248 @@
 const gravity: number = 0.98;
-const jumpForce: number = 20;
-const groundLevel: number = 0;
+const jumpForce: number = 25;
 const maxSpeed: number = 10;
-const baseSpeed: number = 6;
-const forceIncrement: number = 0.1;
-const maxForce: number = 1;
+const baseSpeed: number = 5;
+const forceIncrement: number = 0.2;
+const maxForce: number = 2;
 const airTurnSpeed: number = 3;
 
 interface Vector {
-    x: number;
-    y: number;
+	x: number;
+	y: number;
 }
 
 enum DirectionX {
-    Left = -1,
-    Stop = 0,
-    Right = 1
+	Left = -1,
+	Stop = 0,
+	Right = 1,
+}
+
+enum KeyCodes {
+    Space = 32,
+    ArrowRight = 39,
+    ArrowLeft = 37,
+}
+
+interface Keys {
+    space: boolean;
+    arrowRight: boolean;
+    arrowLeft: boolean;
 }
 
 class Character {
-	private element: HTMLElement;
+    private canvas: HTMLCanvasElement;
+	private color: string;
 	private size: number;
-    private pos: Vector;
-    private vel: Vector;
+	private pos: Vector;
+	private vel: Vector;
 	private forceX: number;
-	private isMovingKeyDown: boolean;
-    private isStopped: boolean;
-    private sign: number;
-    private lastDirection: DirectionX;
+    private keys: Keys;
+	private isStopped: boolean;
+	private sign: number;
+	private lastDirection: DirectionX;
+    private groundLevel: number;
 
-	constructor() {
-		this.size = 32;
-        this.pos = { x: 100, y: 1000 };
-        this.vel = { x: 0, y: 0 };
+	constructor(
+        canvas: HTMLCanvasElement,
+		pos: Vector = { x: 100, y: 1000 },
+		color: string = "red",
+		size: number = 32,
+	) {
+        this.canvas = canvas;
+		this.color = color;
+		this.pos = pos;
+		this.size = size;
+		this.vel = { x: 0, y: 0 };
 		this.forceX = 0;
-		this.isMovingKeyDown = false;
-        this.isStopped = false;
-        this.sign = 1;
-        this.lastDirection = DirectionX.Stop;
+        this.keys = { space: false, arrowRight: false, arrowLeft: false };
+		this.isStopped = false;
+		this.sign = 1;
+		this.lastDirection = DirectionX.Stop;
+        this.groundLevel = this.canvas.height - this.size;
 
-		this.element = document.createElement("div");
-		this.element.className = "character";
-		this.element.style.width = `${this.size}px`;
-		this.element.style.height = `${this.size}px`;
-		this.updateElementPos();
+		this.update();
 
 		this.listeners();
 	}
 
 	listeners() {
 		document.addEventListener("keydown", e => {
-			if (e.key === " ") {
-				this.jump();
-			} else if (e.key === "ArrowRight") {
-                this.move(DirectionX.Right);
-			} else if (e.key === "ArrowLeft") {
-				this.move(DirectionX.Left);
-			}
+			if (e.key === " ")
+                this.keys.space = true;
+			else if (e.key === "ArrowRight") {
+                this.keys.arrowRight = true;
+            }
+			else if (e.key === "ArrowLeft") {
+                this.keys.arrowLeft = true;
+            }
 		});
 
 		document.addEventListener("keyup", e => {
-			if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
-                this.isMovingKeyDown = false;
-			}
+            if (e.key === " ") {
+                this.keys.space = false;
+            }
+            else if (e.key === "ArrowRight") {
+                this.keys.arrowRight = false;
+            }
+            else if (e.key === "ArrowLeft") {
+                this.keys.arrowLeft = false;
+            }
 		});
 	}
 
-    private onGround(): boolean {
-        return this.pos.y <= groundLevel;
+	update() {
+        if (this.keys.space) this.jump();
+        if (this.isBothKeysPressed()) this.stop();
+        else if (this.keys.arrowLeft) this.move(DirectionX.Left);
+        else if (this.keys.arrowRight) this.move(DirectionX.Right);
+        this.nextPos();
+		this.draw();
+	}
+
+    getSize(): number {
+        return this.size;
     }
 
-	getElement(): HTMLElement {
-		return this.element;
-	}
-
-	updateElementPos(): void {
-		this.element.style.left = `${this.pos.x}px`;
-		this.element.style.bottom = `${this.pos.y}px`;
-	}
-
-	updatePosition(): void {
-		this.pos.x += this.vel.x;
-		this.pos.y += this.vel.y;
-		this.updateElementPos();
-	}
-
-	getSize(): number { return this.size; }
+    getPos(): Vector {
+        return this.pos;
+    }
 
 	setPos(x: number, y: number) {
-        this.setX(x);
-        this.setY(y);
+		this.setX(x);
+		this.setY(y);
 	}
 
-	getX(): number { return this.pos.x; }
-    getY(): number { return this.pos.y; }
+	getX(): number {
+		return this.pos.x;
+	}
 
-    setX(x: number) { this.pos.x = x; }
-	setY(y: number) { this.pos.y = y; }
+	getY(): number {
+		return this.pos.y;
+	}
 
-	jump() {
+	setX(x: number) {
+		this.pos.x = x;
+	}
+
+	setY(y: number) {
+		this.pos.y = y;
+	}
+
+	private onGround(): boolean {
+		return this.pos.y >= this.groundLevel;
+	}
+
+	private draw(): void {
+        const ctx = this.canvas.getContext("2d");
+        if (!ctx) throw new Error("Canvas not found");
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.pos.x, this.pos.y, this.size, this.size);
+    }
+
+	private nextPos(): void {
+		this.pos.x += this.vel.x;
+		this.pos.y -= this.vel.y;
+		this.disaccelerate();
+		this.gravityForce();
+	}
+
+	private jump() {
 		if (this.onGround()) {
 			this.vel.y = jumpForce;
-            this.pos.y++;
+			this.pos.y--;
 		}
 	}
 
-    moveStep(direction: DirectionX) {
-        this.isStopped = false;
-        this.isMovingKeyDown = true;
+	private moveStep(direction: DirectionX) {
+		this.isStopped = false;
 
-        this.lastDirection = direction;
-        this.sign = direction;
+		this.lastDirection = direction;
+		this.sign = direction;
 
-        this.vel.x = baseSpeed * this.sign;
-        this.forceX = forceIncrement * this.sign;
-    }
+		this.vel.x = baseSpeed * this.sign;
+		this.forceX = forceIncrement * this.sign;
+	}
 
-	move(direction: DirectionX) {
+	private move(direction: DirectionX) {
+        // First move, needs a first step
+        // like everything in life
         if (this.isStopped) {
             this.moveStep(direction);
             return;
         }
 
-        // If the character is in the air and the direction is different
-        // from the last direction, change the direction and reduce the speed
-        // to simulate air friction
-        if (!this.onGround() && direction !== this.lastDirection) {
-            this.vel.x = airTurnSpeed;
+        // If the direction is different from the last direction
+        // you need to a new step (friction)
+		if (direction !== this.lastDirection) {
+			this.moveStep(direction);
+            // If in the air, reduce the speed to simulate
+            // air friction (you can't stop instantly in the air)
+            if (!this.onGround()) {
+                this.vel.x = airTurnSpeed * this.sign;
+                return;
+            }
             return;
-        }
+		}
 
-        if (direction !== this.lastDirection) {
-            this.moveStep(direction);
-            return;
-        }
 
-        // Limit speed
-        if (Math.abs(this.vel.x) > maxSpeed) {
-            this.vel.x = maxSpeed * this.sign;
-            return;
-        }
+		// Limit speed
+		if (Math.abs(this.vel.x) > maxSpeed) {
+			this.vel.x = maxSpeed * this.sign;
+			return;
+		}
 
-        // Increase force
-        this.forceX += forceIncrement * this.sign;
+        // Check if the moviment is on pressed key same direction
+        this.validateDirection();
 
-        // Limit force
-        if (Math.abs(this.forceX) > maxForce) {
-            this.forceX = maxForce * this.sign;
-        }
+		// Increase force
+		this.forceX += forceIncrement * this.sign;
 
-        // Increase speed
-        this.vel.x += this.forceX;
+		// Limit force
+		if (Math.abs(this.forceX) > maxForce) {
+			this.forceX = maxForce * this.sign;
+		}
+
+		// Increase speed
+		this.vel.x += this.forceX;
 	}
 
-	stop() {
-        this.isStopped = true;
-        this.forceX = 0;
-        this.vel.x = 0;
+	private stop() {
+		this.isStopped = true;
+		this.forceX = 0;
+		this.vel.x = 0;
 	}
 
-	disaccelerate() {
-        if (this.isMovingKeyDown) return;
+    private validateDirection() {
+        // Check velocity and key pressed
+        if (this.vel.x < 0 && !this.keys.arrowLeft ||
+            this.vel.x > 0 && !this.keys.arrowRight) {
 
-        this.forceX = 0;
+            if (this.keys.arrowLeft) this.move(DirectionX.Left);
+            else if (this.keys.arrowRight) this.move(DirectionX.Right);
+            else this.stop();
+        }
+    }
+
+    private isMovingKeyPressed(): boolean {
+        return this.keys.arrowLeft || this.keys.arrowRight;
+    }
+
+    private isBothKeysPressed(): boolean {
+        return this.keys.arrowLeft && this.keys.arrowRight;
+    }
+
+	private disaccelerate() {
+		if (this.isMovingKeyPressed()) return;
+
+		this.forceX = 0;
 		this.vel.x *= 0.3;
-        if (Math.abs(this.vel.x) < 0.1)
-            this.stop();
+		if (Math.abs(this.vel.x) < 0.1) this.stop();
 	}
 
-	gravityForce() {
+	private gravityForce() {
 		if (this.onGround()) {
 			this.vel.y = 0;
-			this.pos.y = groundLevel;
-            return;
+			this.pos.y = this.groundLevel;
 		}
 
 		this.vel.y -= gravity;
@@ -180,57 +250,50 @@ class Character {
 
 	pushAway(limit: number, multiplier: number = 5) {
 		this.pos.x = limit;
-        this.stop();
-        this.updateElementPos();
+		this.stop();
 	}
 
-	update() {
-		this.gravityForce();
-        this.disaccelerate();
-
-		this.updatePosition();
-	}
 }
 
 class Game {
+	private canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D | null;
 	private character: Character;
-	private element: HTMLElement;
-    private leftLimit: number;
-    private rightLimit: number;
+	private leftLimit: number;
+	private rightLimit: number;
 
 	constructor() {
-		this.element = document.createElement("div");
-		this.element.id = "gameContainer";
-		document.body.appendChild(this.element);
-		this.character = new Character();
-		this.element.appendChild(this.character.getElement());
+		this.canvas = document.createElement("canvas");
+		this.canvas.id = "gameContainer";
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.ctx = null;
+        this.character = new Character(this.canvas);
+		this.leftLimit = 50;
+		this.rightLimit = window.innerWidth - this.leftLimit;
 	}
 
 	start() {
-		setInterval(() => {
-			this.update();
-		}, 1000 / 60);
+		document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext("2d");
+        if (!this.ctx) throw new Error("Canvas not found");
+
+        setInterval(() => this.update(), 10);
 	}
 
-    checkLimits() {
-        this.leftLimit = 50
-        this.rightLimit = window.innerWidth - this.character.getSize();
-        this.rightLimit -= this.leftLimit;
-
-		if (this.character.getX() < this.leftLimit)
-			this.character.pushAway(this.leftLimit);
-		else if (this.character.getX() > this.rightLimit)
-			this.character.pushAway(this.rightLimit);
-    }
+	checkLimits() {
+        const size = this.character.getSize();
+		if (this.character.getX() < this.leftLimit) this.character.pushAway(this.leftLimit);
+		else if (this.character.getX() + size > this.rightLimit) this.character.pushAway(this.rightLimit - size);
+	}
 
 	update() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+		this.rightLimit = window.innerWidth - this.leftLimit;
+        this.ctx?.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.checkLimits();
 		this.character.update();
-		
-        if (this.character.getY() < groundLevel) {
-			this.character.setY(groundLevel);
-		}
-
-        this.checkLimits();
 	}
 }
 
